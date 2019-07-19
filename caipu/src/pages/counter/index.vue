a4d23104a0f4b0c80d6a47aeb9137da9eebd3cc8
huzheng12 <742967791@qq.com>
2019-07-19 14:20:25 +0800

    1.0.0

---
+++ caipu/src/pages/counter/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <a href="pages/login/main">a sdfsdfasdf</a>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
---
+++ caipu/src/pages/login/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>99999999</h2>
+  </div>
+</template>

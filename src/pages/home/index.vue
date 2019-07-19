50484db8378a3af5d32641e9d5ad9fdc8fdbe831
huzheng12 <742967791@qq.com>
2019-07-19 21:04:11 +0800

    3.0

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,36 @@
 <template>
     <div>
 
+<<<<<<< HEAD
         <Fenlei></Fenlei>
+=======
+        <div class="flall">
+
+            <ul>
+                <li v-for="(cai,i) in caipuLIst" @click="changenum(i)" :key="i" :class="i==num?'active1':''">
+
+                    <span :class="i==num?'active':''">{{cai.name}}</span>
+                </li>
+
+            </ul>
+
+
+            <div class="right">
+
+                <span v-for="(list,k) in caipuLIst[num].list" :key="k" class="list">
+                    <a :href="'../nav/main?id='+list.id">
+                        {{list.name}}
+                    </a>
+
+                </span>
+
+
+
+
+            </div>
+
+        </div>
+>>>>>>> f7b3214a415c44d57458220efbdd52dae13843f0
 
     </div>
 </template>
@@ -13,10 +42,13 @@
         },
         data() {
             return {
+                caipuLIst: [],
+                num: 2,
 
             }
         },
         created() {
+            const that = this
             wx.request({
                 url: 'https://apis.juhe.cn/cook/category?key=c3d942c200256073ff822ca1dd431f16', //仅为示例，并非真实的接口地址
                 data: {
@@ -27,9 +59,80 @@
                     'content-type': 'application/json' // 默认值
                 },
                 success(res) {
-                    console.log(res.data)
+                    that.caipuLIst = res.data.result
+                    console.log(that.caipuLIst, 222)
                 }
             })
-        }
+        },
+        methods: {
+            changenum(i) {
+                this.num = i
+            }
+        },
+        mounted() {
+            console.log(this.caipuLIst[this.num], 1111)
+        },
     }
+</script>
+<style scoped>
+    .flall {
+        display: flex;
+        position: relative;
+        justify-content: space-between;
+    }
+
+    .flall>ul {
+        width: 30%;
+        box-sizing: border-box;
+        /* margin-left: 8px; */
+        /* color: aliceblue; */
+        text-align: center;
+        height: 1100rpx;
+        overflow-y: scroll;
+
+    }
+
+    .flall>.right {
+        width: 70%;
+        /* display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between; */
+
+    }
+
+    .flall>ul>li {
+        background-color: #e0e0e0;
+        line-height: 80rpx;
+    }
+
+    .active {
+
+        color: #f00;
+        display: inline-block;
+        margin-bottom: 10rpx;
+        border-bottom: 1px solid #f00;
+    }
+
+    .active1 {
+        background-color: #fff !important;
+    }
+
+    .list {
+
+
+        display: inline-block;
+        width: 33%;
+        line-height: 40px;
+        text-align: center;
+    }
+
+    .list-chi {
+        display: block;
+        width: 45%;
+        background-color: #fff;
+    }
+<<<<<<< HEAD
 </script> -->
+=======
+</style>
+>>>>>>> f7b3214a415c44d57458220efbdd52dae13843f0
---
+++ src/pages/nav/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    
+    <div  v-for="(a,i) in arr" :key="i" class="nav">
+    <img :src="a.albums[0]" alt="">
+     <a href="../navTwo/main" class="name">
+       {{a.title}}
+     </a> 
+     <p>
+       <span>功效:&emsp;</span>{{a.tags}}
+     </p>
+     
+     <p>
+     <span>简介:&emsp;</span> {{a.imtro}}
+     </p>
+    </div>
+   
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      arr:[]
+    }
+  },
+  mounted() {
+    const that=this
+     wx.request({
+                url:"https://apis.juhe.cn/cook/index?key=c3d942c200256073ff822ca1dd431f16&cid=10&rn=30", //仅为示例，并非真实的接口地址
+                data: {
+                    x: '',
+                    y: ''
+                },
+                header: {
+                    'content-type': 'application/json' // 默认值
+                },
+                success(res) {
+                    console.log(res.data.result)
+                    that.arr=res.data.result.data
+                }
+            })
+ 
+  },
+}
+</script>
+<style >
+  .nav{
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    border-bottom: 1px dashed #2b2b2b;
+    padding-bottom: 10px;
+    margin: 16px 0;
+  }
+  .name{
+    font-size: 26px;
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+  }
+  .nav p{
+    font-size: 20px;
+    font-weight: 300;
+    color: #2b2b2b;
+    text-align: left;
+  }
+  .nav p>span{
+    font-size: 26px;
+    font-weight: 600;
+    color: #000000;
+    }
+</style>

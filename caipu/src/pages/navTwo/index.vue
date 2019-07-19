3173b9b74132f681236f23043066169e86e3ab22
Gong <1139701003@qq.com>
2019-07-19 19:20:46 +0800

    1.1

---
+++ caipu/src/pages/navTwo/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <h1>2222</h1>
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      arr:[]
+    }
+  },
+  mounted() {
+    // const that=this
+    //  wx.request({
+    //             url:"https://apis.juhe.cn/cook/index?key=c3d942c200256073ff822ca1dd431f16&cid=10&rn=30", //仅为示例，并非真实的接口地址
+    //             data: {
+    //                 x: '',
+    //                 y: ''
+    //             },
+    //             header: {
+    //                 'content-type': 'application/json' // 默认值
+    //             },
+    //             success(res) {
+    //                 console.log(res.data.result)
+    //                 that.arr=res.data.result.data
+    //             }
+    //         })
+ 
+  },
+}
+</script>

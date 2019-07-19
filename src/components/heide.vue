1dd7958d9706af3c99bfcbe8230987694f1d24df
huzheng12 <742967791@qq.com>
2019-07-19 21:00:32 +0800

    2.0

---
+++ src/components/heide.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>99999999</h2>
+    <h2>{{a}}</h2>
+  </div>
+</template>
+<script>
+  export default {
+    props: ["a"],
+    methods: {
+
+    },
+    mounted() {
+      console.log(this.a)
+    },
+  }
+</script>
